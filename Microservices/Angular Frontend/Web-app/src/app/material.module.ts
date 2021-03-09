0d270ee99edc8e3735df18fa27b7798ee0e3694e
Moises <m.lagos1995@gmail.com>
2021-03-09 01:41:17 -0300

    agregando SideNavbar componente

--- Microservices/Angular Frontend/Web-app/src/app/material.module.ts
+++ Microservices/Angular Frontend/Web-app/src/app/material.module.ts
@@ -1,12 +1,24 @@
-import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field'
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+  ],
 })
-
 export class MaterialModule {}
