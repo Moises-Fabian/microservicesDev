a8e3bdb74cef180303b52af85b5b9ad84be2bd99
Moises <m.lagos1995@gmail.com>
2021-03-12 03:28:35 -0300

    trayendo datos del libro al frontend y paginacion realizada

--- Microservices/Angular Frontend/Web-app/src/app/books/book.service.ts
+++ Microservices/Angular Frontend/Web-app/src/app/books/book.service.ts
@@ -1,21 +1,43 @@
 import {Books} from './book.model';
 import {Subject} from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { paginationBooks } from './pagination-books.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class BookService{
+  baseUrl = environment.baseUrl;
 
-  private booksLista: Books[] =[
-
-    {libroId: 1, titulo: 'Algoritmos', descripcion: 'libro basico', autor: 'Juan perez', precio: 25000 },
-    {libroId: 2, titulo: 'Matematicas', descripcion: 'libro avanzado', autor: 'gabriel mendoza', precio: 25000 },
-    {libroId: 3, titulo: 'Aritmetica', descripcion: 'octavo basico', autor: 'luis correa', precio: 25000 },
-    {libroId: 4, titulo: 'Java', descripcion: 'Springboot', autor: 'Aldo Martinez', precio: 25000 },
-
-  ];
+  private booksLista: Books[] =[];
 
   bookSubjet = new Subject<Books>();
 
-  obtenerLibros(){
-    return this.booksLista.slice();
+  bookPagination!: paginationBooks;
+  bookPaginationSubject = new Subject<paginationBooks>();
+
+  constructor(private http: HttpClient){}
+
+  obtenerLibros(libroPorPagina: number, paginaActual: number, sort: string, sortDirection: string, filterValue: any){
+    const request = {
+       pageSize: libroPorPagina,
+       page: paginaActual,
+       sort,
+       sortDirection,
+       filterValue
+    };
+
+    this.http.post<paginationBooks>(this.baseUrl + 'api/Libro/Pagination', request)
+    .subscribe( (Response) =>{
+      this.bookPagination = Response;
+      this.bookPaginationSubject.next(this.bookPagination);
+    });
+  }
+
+  obtenerActualListener(){
+    return this.bookPaginationSubject.asObservable();
   }
 
   guardarLibro(book: Books){
--- Microservices/Angular Frontend/Web-app/src/app/books/books.component.ts
+++ Microservices/Angular Frontend/Web-app/src/app/books/books.component.ts
@@ -1,53 +1,77 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { BookService } from './book.service';
-import {Books} from './book.model';
+import { Books } from './book.model';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatDialog } from '@angular/material/dialog';
 import { BookNuevoComponent } from './book-nuevo.component';
-import {Subject, Subscription} from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
+import { paginationBooks } from './pagination-books.model';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit, AfterViewInit, OnDestroy {
   bookData: Books[] = [];
-  desplegarColumnas = ["titulo", "descripcion", "autor", "precio"];
+  desplegarColumnas = ['titulo', 'descripcion', 'autor', 'precio'];
   dataSource = new MatTableDataSource<Books>();
   @ViewChild(MatSort) ordenamiento!: MatSort;
   @ViewChild(MatPaginator) paginacion;
 
   private bookSubscription!: Subscription;
 
-  constructor(private bookService: BookService, private dialog: MatDialog) { }
-  hacerFiltro(filtro : string){
+  totalLibros = 0;
+  librosporPagina = 2;
+  paginaCombo = [1, 2, 5, 10];
+  paginaActual = 1;
+  sort = 'titulo';
+  sortDirection = 'asc';
+  filterValue = null;
+
+  constructor(private bookService: BookService, private dialog: MatDialog) {}
+
+  eventoPaginador(event: PageEvent){
+    this.librosporPagina = event.pageSize;
+    this.paginaActual = event.pageIndex + 1;
+    this.bookService.obtenerLibros(this.librosporPagina, this.paginaActual, this.sort, this.sortDirection, this.filterValue);
+  }
+
+
+  hacerFiltro(filtro: string) {
     this.dataSource.filter = filtro;
   }
 
-  abrirDialog(){
-    this.dialog.open(BookNuevoComponent,{
-      width : '350px'
+  abrirDialog() {
+    this.dialog.open(BookNuevoComponent, {
+      width: '350px',
     });
   }
 
   ngOnInit(): void {
-    this.dataSource.data = this.bookService.obtenerLibros();
-    this.bookSubscription = this.bookService.bookSubjet.subscribe(() => {
-      this.dataSource.data = this.bookService.obtenerLibros();
-    })
+    this.bookService.obtenerLibros(this.librosporPagina, this.paginaActual, this.sort, this.sortDirection, this.filterValue);
+    this.bookService
+                .obtenerActualListener()
+                .subscribe( (pagination: paginationBooks) => {
+                  this.dataSource = new MatTableDataSource<Books>(pagination.data);
+                  this.totalLibros = pagination.totalRows;
+                });
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.dataSource.sort = this.ordenamiento;
     this.dataSource.paginator = this.paginacion;
-
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.bookSubscription.unsubscribe();
   }
-
 }
