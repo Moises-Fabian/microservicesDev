d8739ef26e9c9efca069d9a28a38d1030ed3115a
Moises <m.lagos1995@gmail.com>
2021-03-23 21:00:11 -0300

    instalacion de librerias y creacion de clases dto para implentar CQRS

---
+++ Microservices/Servicios.api.Seguridad/Core/Application/Register.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Servicios.api.Seguridad.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servicios.api.Seguridad.Core.Application
+{
+    public class Register
+    {
+        public class UsuarioRegisterCommand : IRequest<UsuarioDTO>
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
