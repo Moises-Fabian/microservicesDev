bec27439bfaf95df59b4afbe5d4d18bb0413b7e0
Moises <m.lagos1995@gmail.com>
2021-03-24 21:23:33 -0300

    creacion de logica en login

---
+++ Microservices/Servicios.api.Seguridad/Core/Application/Login.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Servicios.api.Seguridad.Core.DTO;
+using Servicios.api.Seguridad.Core.Entities;
+using Servicios.api.Seguridad.Core.Jwtlogic;
+using Servicios.api.Seguridad.Core.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Servicios.api.Seguridad.Core.Application
+{
+    public class Login
+    {
+        public class UsuarioLoginCommand : IRequest<UsuarioDTO>
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class UsuarioLoginValidation : AbstractValidator<UsuarioLoginCommand>
+        {
+            public UsuarioLoginValidation()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class UsuarioLoginHundler : IRequestHandler<UsuarioLoginCommand, UsuarioDTO>
+        {
+            private readonly SeguridadContexto _context;
+            private readonly UserManager<Usuario> _userManager;
+            private readonly IMapper _mapper;
+            private readonly IJwtGenerator _jwtGenerator;
+            private readonly SignInManager<Usuario> _signInManager;
+
+            public UsuarioLoginHundler(SeguridadContexto context, UserManager<Usuario> userManager, IMapper mapper, IJwtGenerator jwtGenerator, SignInManager<Usuario> signInManager)
+            {
+                _context = context;
+                _userManager = userManager;
+                _mapper = mapper;
+                _jwtGenerator = jwtGenerator;
+                _signInManager = signInManager;
+            }
+            public async Task<UsuarioDTO> Handle(UsuarioLoginCommand request, CancellationToken cancellationToken)
+            {
+                var usuario = await _userManager.FindByEmailAsync(request.Email);
+
+                if (usuario == null)
+                {
+                    throw new Exception("El usuario no existe");
+                }
+
+                var resultado = await _signInManager.CheckPasswordSignInAsync(usuario, request.Password, false);
+
+                if (resultado.Succeeded)
+                {
+                    var usuarioDTO = _mapper.Map<Usuario, UsuarioDTO>(usuario);
+                    usuarioDTO.Token = _jwtGenerator.createToken(usuario);
+                    return usuarioDTO;
+                }
+
+                throw new Exception("Login incorrecto");
+            }
+        }
+    }
+}
